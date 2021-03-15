76eb74fd4472a26ef64914b494d903291d75585a
Martijn Schendstok <sch.martijn@outlook.com>
2021-03-15 14:53:43 +0100

    finished json script with correct format

--- Data preperation/toJSON.py
+++ Data preperation/toJSON.py
@@ -12,13 +12,16 @@ def main(argv):
     df = pd.read_csv('Data/Merged_Verkiezingsuitslag.csv')
     df = df.groupby(['Year', 'OuderRegioNaam']).sum()
 
+    columns = list(df.columns)
+    info_columns = columns[:6]
+    party_columns = columns[6:]
+
     json_text = '{\n' \
                 '"name": "verkiezingsuitslag",\n' \
                 '"children": [\n'
 
     year = 0
     for index, row in df.iterrows():
-        #print(index[0], index[1])
         if index[0] != year:
             if year != 0:
                 json_text += '\n]\n},\n'
@@ -30,14 +33,20 @@ def main(argv):
             json_text += ',\n'
 
         json_text += '{{\n' \
-                     '"name": "{0}",\n' \
-                     '"children": ['.format(index[1])
+                     '"name": "{0}",\n'.format(index[1])
 
-        json_text += ']\n}'
+        for column in info_columns:
+            json_text += '"{0}": {1},\n'.format(column, row[column])
+
+        json_text += '"children": ['
+
+        for column in party_columns:
+            if row[column] != 0:  # Can set a threshold here
+                json_text += '\n{{"name": "{0}", "votes": {1}}},'.format(column, row[column])
+
+        json_text = json_text[:-1] + '\n]\n}'
 
     json_text += '\n]\n}\n]\n}'
-
-    print(json_text)
 
     with open("data.json", "w+") as f:
         f.write(json_text)
