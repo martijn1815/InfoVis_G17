2bea3ebb023a9ac6233a8f29cff95da58e1eb41b
Martijn Schendstok <sch.martijn@outlook.com>
2021-03-24 22:08:07 +0100

    Implement right-left devision of parties

--- Data preperation/toJSON.py
+++ Data preperation/toJSON.py
@@ -6,15 +6,27 @@ Author: Martijn Schendstok
 
 import sys
 import pandas as pd
+from collections import defaultdict
 
 
 def main(argv):
+    print("Loading Data:", end="\t")
+
     df = pd.read_csv('Data/Merged_Verkiezingsuitslag.csv')
     df = df.groupby(['Year', 'OuderRegioNaam']).sum()
+
+    parties = defaultdict(list)
+    with open("parties.csv", "r", encoding='utf-8-sig') as f:
+        for line in f:
+            elements = line.strip().split(";")
+            parties[elements[1]].append(elements[0])
 
     columns = list(df.columns)
     info_columns = columns[:6]
     party_columns = columns[6:]
+
+    print("Done")
+    print("Making JSON-File:", end="\t")
 
     json_text = '{\n' \
                 '"name": "verkiezingsuitslag",\n' \
@@ -38,18 +50,33 @@ def main(argv):
         for column in info_columns:
             json_text += '"{0}": {1},\n'.format(column, row[column])
 
-        json_text += '"children": ['
+        json_text += '"children": [\n'
 
-        for column in party_columns:
-            #if row[column] != 0:  # Can set a threshold here
-                json_text += '\n{{"name": "{0}", "votes": {1}}},'.format(column, row[column])
+        #for column in party_columns:
+        #    #if row[column] != 0:  # Can set a threshold here
+        #        json_text += '\n{{"name": "{0}", "votes": {1}}},'.format(column, row[column])
 
-        json_text = json_text[:-1] + '\n]\n}'
+        for key in parties:
+            json_text += '{{\n' \
+                         '"name": "{0}",\n' \
+                         '"children": ['.format(key)
+
+            for party in parties[key]:
+                json_text += '\n{{"name": "{0}", "votes": {1}}},'.format(party, row[party])
+
+            json_text = json_text[:-1] + '\n]\n},'
+
+        json_text += json_text[:-1] + '\n]\n}'
 
     json_text += '\n]\n}\n]\n}'
 
+    print("Done")
+    print("Writing JSON-File:", end="\t")
+
     with open("data.json", "w+") as f:
         f.write(json_text)
+
+    print("Done")
 
 
 if __name__ == "__main__":
