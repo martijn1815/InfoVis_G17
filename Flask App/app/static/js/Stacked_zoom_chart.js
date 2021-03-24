a5b7b0aef1eda6547ebb50b84159f07460998604
scipiovancrevel <hippescip@gmail.com>
2021-03-24 12:44:51 +0100

    Added title_no container yet

--- Flask App/app/static/js/Stacked_zoom_chart.js
+++ Flask App/app/static/js/Stacked_zoom_chart.js
@@ -1,17 +1,17 @@
 
-
+console.log("TEST: Stackedchart called");
 
 
 function createStackedchart() {
-    console.log("TEST: Stackedchart called");
+
 
     // set the dimensions and margins of the graph
-    var margin = {top: 60, right: 230, bottom: 50, left: 70},
-        width = 660 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    var margin = {top: 60, right: 10, bottom: 10, left: 10},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz")
+    var svg = d3.select("#stacked_bar_chart")
       .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
