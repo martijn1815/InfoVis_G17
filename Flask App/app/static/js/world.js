07d21e06fb6b282af3cde59a54628d22feac6ed7
Martijn Schendstok <sch.martijn@outlook.com>
2021-03-25 19:55:14 +0100

    cleaned up the spacing

--- Flask App/app/static/js/world.js
+++ Flask App/app/static/js/world.js
@@ -17,11 +17,10 @@ function getID(d, x) {
     return ID;
 };
 
-function  updateNLMap(YearID) {
+function updateNLMap(YearID) {
     d3.selectAll("#map_box").remove();
     d3.selectAll("#tooltip").remove();
-    createNLMap(yearid = YearID);
-
+    createNLMap(YearID);
 };
 
 
@@ -31,55 +30,49 @@ function createNLMap(yearid) {
     var YearID2 = yearid
     console.log(yearid)
     var box = d3.select("#map_netherlands")
-        .append("div")
-        .attr("id", "map_box")
+                .append("div")
+                .attr("id", "map_box")
 
     const margin_map = {top: 10, right: 0, bottom: 10, left: 5},
           width_map = 960 - margin_map.left - margin_map.right,
           height_map = 500 - margin_map.top - margin_map.bottom;
 
     var svg = d3.select("#map_box")
-        .append("svg")
-        .attr("id", "#mapsvg")
-        .attr("height", height_map + margin_map.top + margin_map.bottom)
-        .attr("width", width_map + margin_map.left + margin_map.right)
-        .append("g")
-        .attr("id", '#nlmap')
-        .attr("transform", "translate(" + margin_map.left + "," + margin_map.top  + ")");
+                .append("svg")
+                .attr("id", "#mapsvg")
+                .attr("height", height_map + margin_map.top + margin_map.bottom)
+                .attr("width", width_map + margin_map.left + margin_map.right)
+                .append("g")
+                .attr("id", '#nlmap')
+                .attr("transform", "translate(" + margin_map.left + "," + margin_map.top  + ")");
 
     var div = d3.select("main").append("div")
-            .attr("class", "tooltip")
-            .attr("id", "tooltip")
-            .style("opacity", 0);
+                .attr("class", "tooltip")
+                .attr("id", "tooltip")
+                .style("opacity", 0);
 
     d3.queue()
-        .defer(d3.json, "https://cartomap.github.io/nl/wgs84/provincie_2021.geojson")
-        .defer(d3.json, "static/data/data.json")
-        .await(ready);
+      .defer(d3.json, "https://cartomap.github.io/nl/wgs84/provincie_2021.geojson")
+      .defer(d3.json, "static/data/data.json")
+      .await(ready);
 
     function ready(error, topo, data) {
-
-
         var DataYear = data.children[yearid].children;
 
-
         for (var i = 0; i<DataYear.length; i++) {
-
             var ProvinceData = DataYear[i].name;
+            var Max = getMax(DataYear[i].children, "votes");
+
             if (ProvinceData == "FryslÃ¢n") {
                 ProvinceData = "Fryslân"
             }
-            //console.log(DataYear[i].children)
-            var Max = getMax(DataYear[i].children, "votes");
 
             for (var j = 0; j<topo.features.length; j++) {
                 var Province_topo = topo.features[j].properties.statnaam;
 
                 if (ProvinceData == Province_topo) {
-
                     topo.features[j].properties.PartyName = Max.name;
                     topo.features[j].properties.PartyVotes= Max.votes;
-
                     break;
                 }
             }
@@ -91,7 +84,7 @@ function createNLMap(yearid) {
         var scale  = 150;
         var offset = [width_map/2, height_map/2];
         var projection = d3.geoMercator().scale(scale).center(center)
-            .translate(offset);
+                           .translate(offset);
 
         // create the path
         var path = d3.geoPath().projection(projection);
@@ -103,49 +96,45 @@ function createNLMap(yearid) {
         var vscale  = scale*height_map / (bounds[1][1] - bounds[0][1]);
         var scale   = (hscale < vscale) ? hscale : vscale;
         var offset  = [width_map - (bounds[0][0] + bounds[1][0])/2,
-                          height_map - (bounds[0][1] + bounds[1][1])/2];
+                       height_map - (bounds[0][1] + bounds[1][1])/2];
 
         // new projection
         projection = d3.geoMercator().center(center)
-          .scale(scale).translate(offset);
+                       .scale(scale).translate(offset);
         path = path.projection(projection);
 
         var color = d3.scaleOrdinal().range(d3.schemeSet1);
 
         svg.append("g")
-            .selectAll("path")
-            .data(topo.features)
-            .enter()
-            .append("path")
-            // draw each country
-            .attr("d", d3.geoPath()
-                .projection(projection)
-            )
-            .attr("fill", function (d) {
+           .selectAll("path")
+           .data(topo.features)
+           .enter()
+           .append("path")
+           // draw each country
+           .attr("d", d3.geoPath()
+                        .projection(projection)
+           )
+           .attr("fill", function (d) {
                 var Stream = d.properties.PartyName
                 return color(Stream);
-                })
-            .style("stroke", "black")
-            .style("stroke-width_map", "0.002")
-            .on("mouseover", function(d) {
+           })
+           .style("stroke", "black")
+           .style("stroke-width_map", "0.002")
+           .on("mouseover", function(d) {
                 div.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                    div.html(function() {
+                   .duration(200)
+                   .style("opacity", .7);
 
-                    return "<strong>" + d.properties.statnaam + "</strong>" + '<br><br>Biggest party: ' + d.properties.PartyName
-                   })
-                    .style("left", (d3.event.pageX + 50) + "px")
-                    .style("top", (d3.event.pageY - 50) + "px");
-            })
-            .on("mouseout", function(d) {
+                div.html(function() {
+                        return "<strong>" + d.properties.statnaam + "</strong>" + '<br><br>Biggest party: ' + d.properties.PartyName;
+                    })
+                   .style("left", (d3.event.pageX + 50) + "px")
+                   .style("top", (d3.event.pageY - 50) + "px");
+           })
+           .on("mouseout", function(d) {
                 div.transition()
                     .duration(500)
                     .style("opacity", 0);
-            });
-
-
-
+           });
     };
-
 };
